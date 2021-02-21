cbb71417fe531174f9cc65c76c8f4e3d92052d9c
Chau Tran <cctran96@gmail.com>
2021-02-21 01:59:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function theBeatlesPlay(musician, instrument) {
-
+function theBeatlesPlay(musicians, instruments) {
+  let play = []
+  for (let i = 0; i < musicians.length; i++) {
+    play.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return play
 }
